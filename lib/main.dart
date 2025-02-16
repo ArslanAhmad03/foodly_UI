dd875856f42443cef346003e50bc126082ae4bf7
arslan07 <ars0009059@gmail.com>
2025-02-16 14:48:01 +0000

    updateValid

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,186 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:food_ui/constant.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
       title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
       theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: primaryColor,
+            foregroundColor: Colors.white,
+            minimumSize: const Size(double.infinity, 40),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _formKey = GlobalKey<FormState>();
+
+  bool _obscureText = true;
+
+  String? validateEmail(String? email) {
+    RegExp regExp = RegExp(
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+    final isValid = regExp.hasMatch(email ?? '');
+    if (email!.isEmpty) {
+      return 'enter email';
+    } else if (!isValid) {
+      return 'enter valid email';
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: defaultPadding, vertical: defaultPadding / 2),
+          child: Column(
+            children: [
+              TextFormField(
+                validator: validateEmail,
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(hintText: "Email Address"),
+              ),
+              SizedBox(height: defaultPadding),
+              TextFormField(
+                obscureText: _obscureText,
+                onSaved: (value) {},
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'enter password';
+                  }
+                  return null;
+                },
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  suffixIcon: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                    child: _obscureText
+                        ? const Icon(Icons.visibility_off, color: bodyTextColor)
+                        : const Icon(Icons.visibility, color: bodyTextColor),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      print('done');
+                    }
+                  },
+                  child: Text('Sign In')),
+              SizedBox(height: defaultPadding),
+              SocialButton(
+                color: Color(0xFF395998),
+                text: 'connect with facebook',
+                icon: SvgPicture.asset(
+                  'assets/facebook.svg',
+                  colorFilter: const ColorFilter.mode(
+                    Color(0xFF395998),
+                    BlendMode.srcIn,
+                  ),
+                ),
+                press: () {
+                  print('facebook');
+                },
+              ),
+              SizedBox(height: defaultPadding),
+              SocialButton(
+                color: Color(0xFF4285F4),
+                text: 'connect with facebook',
+                icon: SvgPicture.asset(
+                  'assets/google.svg',
+                ),
+                press: () {
+                  print('facebook');
+                },
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    );
+  }
+}
+
+class SocialButton extends StatelessWidget {
+  final Color color;
+  final String text;
+  final Widget icon;
+  final GestureTapCallback press;
+  SocialButton(
+      {super.key,
+      required this.color,
+      required this.text,
+      required this.icon,
+      required this.press});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          backgroundColor: color,
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(8))),
+        ),
+        onPressed: press,
+        child: Row(
+          children: [
+            Container(
+                padding: const EdgeInsets.all(8),
+                height: 28,
+                width: 28,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(4)),
+                ),
+                child: icon),
+            const Spacer(flex: 2),
+            Text(
+              text.toUpperCase(),
+              style: Theme.of(context)
+                  .textTheme
+                  .bodySmall!
+                  .copyWith(color: Colors.white, fontWeight: FontWeight.w600),
+            ),
+            const Spacer(flex: 3),
+          ],
         ),
       ),
     );
